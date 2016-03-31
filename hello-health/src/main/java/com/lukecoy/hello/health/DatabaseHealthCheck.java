c2875c2993d04888f9c402fb1852e56eaf875d72
Luke Coy <coy.luke@gmail.com>
2016-03-31 11:33:08 -0400

    Add TODO to health check

--- hello-health/src/main/java/com/lukecoy/hello/health/DatabaseHealthCheck.java
+++ hello-health/src/main/java/com/lukecoy/hello/health/DatabaseHealthCheck.java
@@ -3,7 +3,9 @@ package com.lukecoy.hello.health;
 import com.hubspot.dropwizard.guice.InjectableHealthCheck;
 
 public class DatabaseHealthCheck extends InjectableHealthCheck {
-
+  
+  // TODO: Actually provide a real health check
+  
   @Override
   protected Result check() {
     return Result.healthy();
